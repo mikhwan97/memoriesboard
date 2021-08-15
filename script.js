4a8506ad78b97c5f6b8bd6fc4a02861d6721bf82
mikhwan97 <74431455+mikhwan97@users.noreply.github.com>
2021-08-15 23:07:05 +0800

    initialcommit

---
+++ script.js
@@ -0,0 +1,23 @@
+//jQuery to make element dissapear slowly on a click of a button . Without CSS!
+$(document).ready(function() {
+    var target = $(".dissapear");
+    var target2= $(".appear")
+    $(".arrow-button").click(function() {
+      removeElement(target);
+      addElement(target2);
+    });
+  });
+  
+  function removeElement(target) {
+    target.animate({
+      opacity: "-=1"
+    }, 1000, function() {
+      target.remove();
+    });
+  }
+
+  function addElement(target) {
+    target.delay(800).animate({
+      opacity: "+=1"
+    });
+  }
